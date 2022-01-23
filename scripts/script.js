e1262f8e6ea9126ab4557bd1ea92ff4e808e0bff
Bruno <bruno.dev8801@gmail.com>
2022-01-23 19:59:38 -0300

    created section banner and added header animation script

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,70 @@
-// {
-//   let element = document.querySelector("main");
-//   let header = document.querySelector("header");
-//   function animatePage() {
-//     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//     if (element.offsetTop < scrollTop) {
-//       header.style.visibility = "visible";
-//       header.style.opacity = "1";
-//       header.style.top = "0";
-//     } else {
-//       header.style.visibility = "hidden";
-//       header.style.opacity = "0";
-//       header.style.top = "-18rem";
-//     }
-//   }
-//   window.addEventListener("scroll", animatePage);
-// }
-{
-  let aside = document.querySelector("header .aside-menu");
-  let menuButton = document.querySelector("header .aside-button");
-  function toggleMenu() {
-    if (
-      aside.classList.contains("asideMenu-active") &&
-      menuButton.classList.contains("asideButton-active")
-    ) {
-      aside.classList.remove("asideMenu-active");
-      menuButton.classList.remove("asideButton-active");
+// Header Scroll Animate
+let lastScrollTop = 0;
+let header = document.querySelector("header");
+
+let Header = {
+  hideAndShowHeader: () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+      header.style.top = "-7rem";
     } else {
-      aside.classList.add("asideMenu-active");
-      menuButton.classList.add("asideButton-active");
+      header.style.top = "0";
     }
-  }
-  document
-    .querySelector("header .aside-button")
-    .addEventListener("click", toggleMenu);
-}
+    return (lastScrollTop = scrollTop);
+  },
+};
+window.addEventListener("scroll", Header.hideAndShowHeader);
+
+// Menu de Categorias
+let menuCategory = {
+  aside: document.querySelector("header .aside-menu"),
+  menuButton: document.querySelector("header .aside-button"),
+  toggleMenu: function () {
+    if (
+      menuCategory.aside.classList.contains("asideMenu-active") &&
+      menuCategory.menuButton.classList.contains("asideButton-active")
+    ) {
+      menuCategory.aside.classList.remove("asideMenu-active");
+      menuCategory.menuButton.classList.remove("asideButton-active");
+    } else {
+      menuCategory.aside.classList.add("asideMenu-active");
+      menuCategory.menuButton.classList.add("asideButton-active");
+    }
+  },
+  eventButtonClick: () => {
+    document
+      .querySelector("header .aside-button")
+      .addEventListener("click", menuCategory.toggleMenu);
+  },
+};
+menuCategory.eventButtonClick();
+
+// Banner Principal
+let currentSlider = 0;
+let totalSlides = document.querySelectorAll(
+  "main #banner .slider .slider-item"
+).length;
+document.querySelector(
+  "main #banner .slider"
+).style.width = `calc(100vw * ${totalSlides})`;
+let slider = {
+  goBack: () => {
+    currentSlider--;
+    currentSlider < 0 ? (currentSlider = totalSlides - 1) : currentSlider;
+    slider.updateMargin();
+  },
+  goForward: () => {
+    currentSlider++;
+    currentSlider > totalSlides - 1 ? (currentSlider = 0) : currentSlider;
+    slider.updateMargin();
+  },
+  updateMargin: () => {
+    let newMargin = currentSlider * 100;
+    document.querySelector(
+      "main #banner .slider"
+    ).style.marginLeft = `-${newMargin}vw`;
+  },
+};
+setInterval(() => {
+  slider.goForward();
+}, 7000);
